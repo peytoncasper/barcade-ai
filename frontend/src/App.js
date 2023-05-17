797910d932650881da0dbbb0d00be309b9502b55
Peyton Casper <peytoncas@gmail.com>
2023-05-17 11:56:27 -0700

    removed unused dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,10 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import {Fragment, useEffect, useState} from 'react'
-import { Dialog, Menu, Transition } from '@headlessui/react'
+import {Fragment, useState} from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import {
-    Bars3Icon,
-    BellIcon,
-    CalendarIcon,
-    ChartPieIcon,
-    Cog6ToothIcon,
-    DocumentDuplicateIcon,
-    FolderIcon,
     HomeIcon,
     UsersIcon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import TowerOfHanoi from "./TowersOfHanoi";
-import TurnList from "./TurnList";
-import io from 'socket.io-client';
 import {Outlet} from "react-router-dom";
 
 
